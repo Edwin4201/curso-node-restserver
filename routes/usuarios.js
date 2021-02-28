476fc1e07b286a16b95f8629f84adb233b6da490
EdwinMC451 <64909449+EdwinMC451@users.noreply.github.com>
2021-02-27 23:02:04 -0600

    JWT implementado

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
+const {
+    validarCampos,
+    validarJWT,
+    esAdminRole,
+    tieneRole
+} = require('../middlewares');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
 
 const {
@@ -36,6 +42,9 @@ router.post('/', [
 ], usuariosPost);
 
 router.delete('/:id', [
+    validarJWT,
+    // esAdminRole,
+    tieneRole('ADMIN_ROLE', 'VENTAR_ROLE', 'OTRO_ROLE'),
     check('id', 'No es un ID válido').isMongoId(),
     check('id').custom(existeUsuarioPorId),
     validarCampos
